76105a192d32a3caf56766b6eb6bdd3ef1871c07
sahebbali <saheb1996jmr@gmail.com>
2025-10-25 15:20:14 +0600

    integrate top up history page

--- src/app/(dashboard)/user/(top-up)/top-up-history/page.jsx
+++ src/app/(dashboard)/user/(top-up)/top-up-history/page.jsx
@@ -2,95 +2,99 @@
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
-// import { getUserDepositHistory } from "@/lib/actions/deposit.action";
+import { getPackageBuyInfo } from "@/lib/actions/packageBuyInfoActions";
 
-const LevelOnePage = async ({ searchParams }) => {
+const TOPUPHistoryPage = async ({ searchParams }) => {
   const page = searchParams.page ? parseInt(searchParams.page) : 1;
   const limit = 10;
-  console.log({ page, limit });
+  // console.log({ page, limit });
+  const res = await getPackageBuyInfo(page, limit);
+  // console.log({ res });
+  const data = res.success ? res.data : [];
+  const total = res.success ? res.total : 0;
 
-  const total = 20;
-
+  // console.log({ data });
   const columns = [
     { header: "Sl", accessor: "sl" },
     { header: "User ID", accessor: "userId" },
     { header: "Full Name", accessor: "fullName" },
-    { header: "Amount", accessor: "amount" },
-    { header: "Activation Date", accessor: "activationDate" },
+    { header: "Amount", accessor: "packageAmount" },
+    { header: "Activation Date", accessor: "date" },
+    { header: "Status", accessor: "status" },
   ];
 
-  const data = [
-    {
-      sl: 1,
-      userId: "AB050820250001",
-      fullName: "John Doe",
-      amount: 1000,
-      activationDate: "2024-06-15",
-    },
-    {
-      sl: 2,
-      userId: "AB050820250002",
-      fullName: "Jane Smith",
-      amount: 1000,
-      activationDate: "2024-06-15",
-    },
-    {
-      sl: 3,
-      userId: "AB050820250003",
-      fullName: "Michael Johnson",
-      amount: 1000,
-      activationDate: "2024-06-15",
-    },
-    {
-      sl: 4,
-      userId: "AB050820250004",
-      fullName: "Emily Davis",
-      amount: 1000,
-      activationDate: "2024-06-15",
-    },
-    {
-      sl: 5,
-      userId: "AB050820250005",
-      fullName: "David Brown",
-      amount: 1000,
-      activationDate: "2024-06-15",
-    },
-    {
-      sl: 6,
-      userId: "AB050820250006",
-      fullName: "Sophia Wilson",
-      amount: 1000,
-      activationDate: "2024-06-15",
-    },
-    {
-      sl: 7,
-      userId: "AB050820250007",
-      fullName: "Daniel Miller",
-      amount: 1000,
-      activationDate: "2024-06-15",
-    },
-    {
-      sl: 8,
-      userId: "AB050820250008",
-      fullName: "Olivia Taylor",
-      amount: 1000,
-      activationDate: "2024-06-15",
-    },
-    {
-      sl: 9,
-      userId: "AB050820250009",
-      fullName: "James Anderson",
-      amount: 1000,
-      activationDate: "2024-06-15",
-    },
-    {
-      sl: 10,
-      userId: "AB050820250010",
-      fullName: "Ava Thomas",
-      amount: 1000,
-      activationDate: "2024-06-15",
-    },
-  ];
+  // const data = [
+  //   {
+  //     sl: 1,
+  //     userId: "AB050820250001",
+  //     fullName: "John Doe",
+  //     amount: 1000,
+  //     activationDate: "2024-06-15",
+  //   },
+  //   {
+  //     sl: 2,
+  //     userId: "AB050820250002",
+  //     fullName: "Jane Smith",
+  //     amount: 1000,
+  //     activationDate: "2024-06-15",
+  //   },
+  //   {
+  //     sl: 3,
+  //     userId: "AB050820250003",
+  //     fullName: "Michael Johnson",
+  //     amount: 1000,
+  //     activationDate: "2024-06-15",
+  //   },
+  //   {
+  //     sl: 4,
+  //     userId: "AB050820250004",
+  //     fullName: "Emily Davis",
+  //     amount: 1000,
+  //     activationDate: "2024-06-15",
+  //   },
+  //   {
+  //     sl: 5,
+  //     userId: "AB050820250005",
+  //     fullName: "David Brown",
+  //     amount: 1000,
+  //     activationDate: "2024-06-15",
+  //   },
+  //   {
+  //     sl: 6,
+  //     userId: "AB050820250006",
+  //     fullName: "Sophia Wilson",
+  //     amount: 1000,
+  //     activationDate: "2024-06-15",
+  //   },
+  //   {
+  //     sl: 7,
+  //     userId: "AB050820250007",
+  //     fullName: "Daniel Miller",
+  //     amount: 1000,
+  //     activationDate: "2024-06-15",
+  //   },
+  //   {
+  //     sl: 8,
+  //     userId: "AB050820250008",
+  //     fullName: "Olivia Taylor",
+  //     amount: 1000,
+  //     activationDate: "2024-06-15",
+  //   },
+  //   {
+  //     sl: 9,
+  //     userId: "AB050820250009",
+  //     fullName: "James Anderson",
+  //     amount: 1000,
+  //     activationDate: "2024-06-15",
+  //   },
+  //   {
+  //     sl: 10,
+  //     userId: "AB050820250010",
+  //     fullName: "Ava Thomas",
+  //     amount: 1000,
+  //     activationDate: "2024-06-15",
+  //   },
+  // ];
 
   const renderRow = (item, index) => (
     <tr
@@ -103,12 +107,25 @@ const LevelOnePage = async ({ searchParams }) => {
       </td>
       <td className="whitespace-nowrap">{item.userId}</td>
       <td className="whitespace-nowrap">{item.fullName}</td>
-      <td className="whitespace-nowrap">${item.amount}</td>
-      <td className="whitespace-nowrap">${item.activationDate}</td>
+      <td className="whitespace-nowrap">${item.packageAmount}</td>
+      <td className="whitespace-nowrap">
+        {new Date(item.date).toDateString()}
+      </td>
+      <td>
+        <span
+          className={`px-2 py-1 rounded text-xs font-semibold ${
+            item.status.toLowerCase() === "success"
+              ? "bg-green-100 text-green-600"
+              : item.status.toLowerCase() === "rejected"
+              ? "bg-red-100 text-red-600"
+              : "bg-yellow-100 text-yellow-600"
+          }`}
+        >
+          {item.status}
+        </span>
+      </td>
     </tr>
   );
-
-  // const page = searchParams.page ? parseInt(searchParams.page) : 1;
 
   return (
     <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
@@ -133,4 +150,4 @@ const LevelOnePage = async ({ searchParams }) => {
   );
 };
 
-export default LevelOnePage;
+export default TOPUPHistoryPage;
